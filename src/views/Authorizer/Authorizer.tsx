c80549720d7bea7408821db1d6cd4b0373925578
Gimmy <gheorghe.popescu@aserto.com>
2025-04-15 11:42:43 +0300

    use react-router Outlet for navigation

--- src/views/Authorizer/Authorizer.tsx
+++ src/views/Authorizer/Authorizer.tsx
@@ -1,11 +1,12 @@
-import React, { PropsWithChildren } from 'react'
+import React from 'react'
+import { Outlet } from 'react-router'
 
 import PaddedContent from '../../components/common/PaddedContent'
 import HeaderComponent from './Header'
 import AuthorizerSidebar from './Sidebar'
 import { Content, Grid, Sidebar } from './styles'
 
-const Frame: React.FC<PropsWithChildren<object>> = ({ children }) => {
+const Frame: React.FC = () => {
   return (
     <PaddedContent>
       <Grid>
@@ -13,7 +14,9 @@ const Frame: React.FC<PropsWithChildren<object>> = ({ children }) => {
         <Sidebar>
           <AuthorizerSidebar />
         </Sidebar>
-        <Content>{children}</Content>
+        <Content>
+          <Outlet />
+        </Content>
       </Grid>
     </PaddedContent>
   )
--- src/views/Authorizer/index.tsx
+++ src/views/Authorizer/index.tsx
@@ -3,7 +3,8 @@ import './index.css'
 import { Suspense } from 'react'
 import { Navigate, Route, Routes } from 'react-router'
 
-import Frame from './Frame'
+import Frame from './Authorizer'
+import Authorizer from './Authorizer'
 import AuthorizerApiDocs from './pages/ApiDocs'
 import Evaluator from './pages/Evaluator'
 import Modules from './pages/Modules'
@@ -12,17 +13,16 @@ const PolicyInstanceDetails = () => {
   return (
     <Suspense fallback={<Frame />}>
       <Routes>
-        <Route element={<Evaluator />} path="/evaluator" />
-        <Route element={<Modules />} path="/modules" />
-        <Route
-          element={
-            <Frame>
-              <AuthorizerApiDocs />
-            </Frame>
-          }
-          path="/docs"
-        />
-        <Route element={<Navigate to="modules" />} path="*" />
+        <Route element={<Authorizer />} path="evaluator">
+          <Route element={<Evaluator />} path="" />
+        </Route>
+        <Route element={<Authorizer />} path="modules">
+          <Route element={<Modules />} path="" />
+        </Route>
+        <Route element={<Authorizer />} path="docs">
+          <Route element={<AuthorizerApiDocs />} path="" />
+        </Route>
+        <Route element={<Navigate replace to="modules" />} index />
       </Routes>
     </Suspense>
   )
