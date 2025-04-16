5c8bfbffb5109000162181791b2e023e7b646a33
Gimmy <gheorghe.popescu@aserto.com>
2025-04-16 11:27:58 +0300

    upload to codecov

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 
+import { codecovVitePlugin } from "@codecov/vite-plugin";
 import react from '@vitejs/plugin-react-swc'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), codecovVitePlugin({
+    bundleName: "topaz-console",
+    enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+    uploadToken: process.env.CODECOV_TOKEN,
+  }),],
   server: {
     port: 3000,
   },
